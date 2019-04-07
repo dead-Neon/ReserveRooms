59b5a940c778733f59c344d8e0f26d268e8dfb71
Max <MaxArastir@gmail.com>
2019-04-07 09:43:52 +0100

    Stuff

--- ReserveRoom/src/application/Booking.java
+++ ReserveRoom/src/application/Booking.java
@@ -1,15 +1,37 @@
 package application;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Booking {
-	
+	final SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
+	DateFormat dateFormat = DateFormat.getDateTimeInstance( DateFormat.DEFAULT,DateFormat.DEFAULT, locale);
 	String fname, lname;
 	ArrayList<Date> dates = new ArrayList<>();
 	boolean checkIn, checkOut;
 	
+	
+	
+	public static String getNextDate(String  curDate) {
+		  final SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		  Date date = null;
+		try {
+			date = format.parse(curDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		  final Calendar calendar = Calendar.getInstance();
+		  calendar.setTime(date);
+		  calendar.add(Calendar.DAY_OF_YEAR, 1);
+		  return format.format(calendar.getTime()); 
+		}
+	
 	public boolean isCheckIn() {
+		
 		return checkIn;
 	}
 
@@ -29,9 +51,16 @@ public class Booking {
 		this.dates = dates;
 	}
 
-	Booking(ArrayList<Date> dates, String fname, String lname)
+	Booking(Date startDate, Date leaveDate, String fname, String lname)
 	{
-		this.dates = dates;
+		ArrayList<Date> dates = new ArrayList<>();
+		dates.add(startDate);
+		dates.add(leaveDate);
+		while (startDate != leaveDate)
+		{
+			startDate = getNextDate(startDate);
+		}
+		
 		this.fname = fname;
 		this.lname = lname;
 	}
